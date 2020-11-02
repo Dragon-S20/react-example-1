e505672c2963961de96a29e2d86e97c6d8a90856
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-11-02 12:14:43 -0500

    done with chakra

--- src/layouts/FetchTab.js
+++ src/layouts/FetchTab.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import { Heading } from '@chakra-ui/core'
+import axios from 'axios'
 
 function FetchTab() {
-  return <Heading>Fetch content with useEffect hook</Heading>
-}
-
-export default FetchTab
+  const [data, setData] = useState({ hits: [] })
+  const [query, setQuery] = useState('redux')
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+  );
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(url);
+    
+ 
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, [url]);
+  
+  return (
+    <>
+    <Heading>Fetch content with useEffect hook</Heading>
+    <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+  <button type="button" onClick={() => setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)}>
+        Search
+      </button>
+  <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+    </>
+  );
+      }
+export default FetchTab;
