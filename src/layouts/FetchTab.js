1c5a35b24f54ff611483a1a1857873d7a142416a
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-11-03 03:20:24 -0500

    chakra

--- src/layouts/FetchTab.js
+++ src/layouts/FetchTab.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from 'react'
-import { Heading } from '@chakra-ui/core'
+import { Heading, Button, Input, Stack, HStack, VStack, Box } from '@chakra-ui/core'
 import axios from 'axios'
 
 function FetchTab() {
@@ -22,22 +22,32 @@ function FetchTab() {
   
   return (
     <>
-    <Heading>Fetch content with useEffect hook</Heading>
-    <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-  <button type="button" onClick={() => setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)}>
-        Search
-      </button>
-  <ul>
+    <VStack spacing={30}>
+    <Heading mb={50}>Fetch content with useEffect hook</Heading>
+    <HStack>
+    <Stack spacing={3}>
+  <Input type="text" variant="flushed" placeholder="Flushed" value={query}
+        onChange={event => setQuery(event.target.value)} />
+  </Stack>
+    <Button
+      colorScheme="purple"
+      color="red.400"
+      onClick={() => setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      }
+    >
+      Search
+    </Button>
+    </HStack>
+    <Box spacing={40}>
+    <ul>
       {data.hits.map(item => (
         <li key={item.objectID}>
           <a href={item.url}>{item.title}</a>
         </li>
       ))}
     </ul>
+</Box>
+    </VStack>
     </>
   );
       }
